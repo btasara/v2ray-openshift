34433714b0b2adf9f09320be83965034ac33e141
btasara <85505173+btasara@users.noreply.github.com>
2021-06-25 13:54:46 +0200

    update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # V2Ray new configuration
-cat <<-EOF > /v2ray/config.json
+cat <<-EOF > v2ray/config.json
 $CONFIG_JSON
 EOF
 # Run V2Ray
-exec /v2ray/v2ray -config=/v2ray/config.json
+exec v2ray/v2ray -config=v2ray/config.json
